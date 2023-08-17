be4b026571859e8063efde8524822f7b78389594
mohsinali <mohammadmohsin@gmail.com>
2023-08-17 08:13:28 +0500

    AdminKit theme implemented with Rails 7

---
+++ app/javascript/application.js
@@ -0,0 +1,24 @@
+// Entry point for the build script in your package.json
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+// import * as bootstrap from "bootstrap"
+
+
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+import * as bootstrap from "bootstrap"
+
+import "./modules/sidebar"
+import "./modules/theme"
+import "./modules/feather"
+
+// Charts
+import "./modules/chartjs"
+
+// Forms
+import "./modules/flatpickr"
+
+// Maps
+import "./modules/vector-maps"
